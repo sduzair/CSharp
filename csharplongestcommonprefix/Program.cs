3988dcc5064cc46fed63252dbdd6583ce84b470f
sduzair <sduzairahmed@gmail.com>
2023-02-05 01:33:18 -0500

    2023-02-04

---
+++ csharplongestcommonprefix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetLongestCommonPrefix(args));
+    }
+    private static string GetLongestCommonPrefix(string[] words)
+    {
+        string smallestWord = GetSmallestWord(words);
+        StringBuilder longestCommonPrefix = new();
+        Boolean tag = true;
+        for (int i = 0; i < smallestWord.Length; i++)
+        {
+            foreach (var word in words)
+            {
+                if (word[i] != smallestWord[i]) tag = false; 
+            }
+            if (!tag) break;
+            longestCommonPrefix.Append(smallestWord[i]);
+        }
+        return longestCommonPrefix.ToString();
+    }
+
+    private static string GetSmallestWord(string[] words)
+    {
+        int min = int.MaxValue;
+        StringBuilder smallestWord = new();
+
+        foreach (var word in words)
+        {
+            if (min > word.Length)
+            {
+                min = word.Length;
+                smallestWord.Clear();
+                smallestWord.Append(word);
+            }
+        }
+
+        return smallestWord.ToString();
+    }
+}
---
+++ csharpvalidparentheses/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+    Algorithm:
+    
+    var bracketStack = new Stack<char>();
+    boolean isValid = true;
+    foreach ( char in string && isValid)
+        if (isOpeningBracket(char))
+            bracketStack.push(char)
+        else if (isClosingBracket(char))
+            isValid = isBracketPair(bracketStack.pop(), char) ? true : false
+        else if 
+            isValid = false
+            
+*/
+
+internal class Program
+{
+    private static readonly Dictionary<char, char> BracketPairs = new()
+    {
+        { '{', '}' },
+        { '[', ']' },
+        { '(', ')' },
+        { '<', '>' },
+    };
+    private static void Main(string[] args)
+    {
+        if (IsValidString(args[0])) Console.WriteLine("String is Valid");
+        else Console.WriteLine("String is Invalid");
+        
+    }
+
+    private static Boolean IsValidString(string word)
+    {
+        var bracketStack = new Stack<char>();
+        bool isValid = true;
+
+        foreach (var letter in word) if(isValid)
+        {
+            if (IsOpeningBracket(letter))
+                bracketStack.Push(letter);
+            else if (IsClosingBracket(letter))
+            {
+                if (bracketStack.Count == 0)
+                {
+                    isValid = false;
+                        break;
+                }
+                isValid = IsBracketPair(bracketStack.Pop(), letter) ? true : false;
+            }
+            else 
+                isValid = false;
+        }
+
+        if (bracketStack.Count > 0) isValid = false;
+
+        return isValid;
+    }
+
+    private static bool IsBracketPair(char v, char letter)
+    {
+        if (BracketPairs[v] == letter) return true;
+        else return false;
+    }
+
+    private static bool IsClosingBracket(char letter)
+    {
+        return BracketPairs.ContainsValue(letter);
+    }
+
+    private static bool IsOpeningBracket(char letter)
+    {
+        return BracketPairs.ContainsKey(letter);
+    }
+}
+
