8bdfaada044842bb40cc6e1bfd8fd88dda871fc8
sduzair <sduzairahmed@gmail.com>
2023-09-01 19:59:09 -0400

    prints credit scores also

--- csharpstudentgrades/Program.cs
+++ csharpstudentgrades/Program.cs
@@ -4,12 +4,13 @@ internal class Program
 {
     private const int NUMBER_OF_SUBJECT_SCORES = 5;
     private const int NUMBER_OF_EXTRACREDIT_SCORES = 3;
+    private const int NUMBER_OF_STUDENTS = 8;
 
     private static void Main(string[] args)
     {
-        var studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+        var studentNames = new string[NUMBER_OF_STUDENTS] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
-        var studentScores = new int[8, NUMBER_OF_SUBJECT_SCORES + NUMBER_OF_EXTRACREDIT_SCORES]
+        var studentScores = new int[NUMBER_OF_STUDENTS, NUMBER_OF_SUBJECT_SCORES + NUMBER_OF_EXTRACREDIT_SCORES]
         {
             { 90, 86, 87, 98, 100, 94, 90, 0 },
             { 92, 89, 81, 96, 90, 89, 0, 0 },
@@ -21,30 +22,37 @@ internal class Program
             { 91, 91, 91, 91, 91, 91, 91, 0 }
         };
 
-        Console.WriteLine($"Student{GetNumberOfTabs("Student")}Average{GetNumberOfTabs("Average")}Grade\n");
+        Console.WriteLine($"Student{GetNumberOfTabs("Student")}Exam Score{GetNumberOfTabs("Exam Score")}Overall{GetNumberOfTabs("Average")}Grade{GetNumberOfTabs("Grade")}Extra Credit\n");
 
         for (int i = 0; i < studentNames.Length; i++)
         {
             //calculate average of scores
             int sum = GetSumOfSubjectScores(studentScores, i);
 
-            double extraCreditScore = GetExtraCreditScore(studentScores, i);
+            decimal examScore = (decimal)sum / 5;
 
-            double average = (sum + extraCreditScore) / 5;
+            decimal totalCreditScore = GetTotalCreditScore(studentScores, i);
 
-            string letterGrade = GetLetterGrade(average);
+            decimal extraCreditScore = totalCreditScore * 0.10m;
 
-            Console.WriteLine($"{studentNames[i]}{GetNumberOfTabs(studentNames[i])}{average}{GetNumberOfTabs($"{average}")}{letterGrade}");
+            decimal extraCreditPoints = totalCreditScore * 0.10m / 5;
+
+            decimal finalNumericScore = (sum + totalCreditScore * 0.10m) / 5;
+
+            string letterGrade = GetLetterGrade(finalNumericScore);
+
+            Console.WriteLine($"{studentNames[i]}{GetNumberOfTabs(studentNames[i])}{examScore}{GetNumberOfTabs($"{examScore}")}{finalNumericScore}{GetNumberOfTabs($"{finalNumericScore}")}{letterGrade}{GetNumberOfTabs(letterGrade)}{extraCreditScore}({extraCreditPoints})");
         }
 
         static string GetNumberOfTabs(string v, int maxStringLength = 12)
         {
             int maxTabs = (int)Math.Ceiling((double)maxStringLength / 4);
 
-            return new('\t', maxTabs - v.Length / 4);
+            int occupiedTabSpaces = v.Length / 4 == 0 ? 1 : v.Length / 4;
+            return new('\t', maxTabs - occupiedTabSpaces);
         }
 
-        static string GetLetterGrade(double average)
+        static string GetLetterGrade(decimal average)
         {
             string letterGrade;
             if (average >= 97)
@@ -100,14 +108,14 @@ internal class Program
         return sum;
     }
 
-    private static double GetExtraCreditScore(int[,] studentScores, int i)
+    private static decimal GetTotalCreditScore(int[,] studentScores, int i)
     {
-        double extraCreditScore = 0;
+        decimal totalCreditScore = 0;
         for (int j = NUMBER_OF_SUBJECT_SCORES; j < NUMBER_OF_SUBJECT_SCORES + NUMBER_OF_EXTRACREDIT_SCORES; j++)
         {
-            extraCreditScore += studentScores[i, j] / 10;
+            totalCreditScore += studentScores[i, j];
         }
 
-        return extraCreditScore;
+        return totalCreditScore;
     }
 }
