824feb0c4f1af2f0091f0d7b853723416c79a55a
sduzair <sduzairahmed@gmail.com>
2023-09-02 01:07:53 -0400

    fizzbuzzbazz

---
+++ csharpfizzbuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿var divisors = new Dictionary<int, string>()
+{
+    { 3, "Fizz" },
+    { 5, "Buzz" },
+    { 7, "Bazz" },
+};
+
+for (int i = 1; i <= 100; i++)
+{
+    System.Console.Write($"{i} - ");
+    foreach (int key in divisors.Keys)
+    {
+        if (i % key == 0)
+        {
+            System.Console.Write($"{divisors[key]}");
+        }
+    }
+    System.Console.WriteLine();
+}
