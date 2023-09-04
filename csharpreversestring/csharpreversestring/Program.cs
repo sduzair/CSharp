7e354cfaed351b84fa2aefedfcedac727ebf91ea
sduzair <sduzairahmed@gmail.com>
2023-09-03 22:06:41 -0400

    reverse string

---
+++ csharpreversestring/csharpreversestring/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Enter \"exit\" to stop reversing strings");
+do
+{
+    string? word;
+    do
+    {
+        word = Console.ReadLine();
+    } while (string.IsNullOrEmpty(word));
+
+    if (word.Trim().ToLower() == "exit") break;
+
+    string reverse = ReverseString(word);
+
+    System.Console.WriteLine($"{word} -> {reverse}");
+
+} while (true);
+
+static string ReverseString(string word)
+{
+    if (word.Length == 1) return word;
+    return word.Substring(word.Length - 1) + ReverseString(word.Substring(0, word.Length - 1));
+}
