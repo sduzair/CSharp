e1cca7c99c331d338ee9b5b79ce2eeefa06c68e1
sduzair <sduzairahmed@gmail.com>
2023-09-03 22:06:02 -0400

    casting converting

---
+++ csharpcastingconverting/Program.cs
@@ -0,0 +1,10 @@
+﻿// Casting -> Truncates
+int value = (int)1.5m;
+System.Console.WriteLine(value);    // Output: 1
+
+// invalid - parse works for string as input 
+// int value2 = int.TryParse(1.5m, out value2);
+
+// Converting -> Rounds
+int value3 = Convert.ToInt32(1.5m);
+System.Console.WriteLine(value3);   // Output: 2
