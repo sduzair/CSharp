d6d53665495a1c47312483cef6c6e7d91e677395
sduzair <sduzairahmed@gmail.com>
2023-09-05 18:53:40 -0400

    conditions exhaustive and exclusive debug.assert

--- csharpdowhile/Program.cs
+++ csharpdowhile/Program.cs
@@ -1,26 +1,73 @@
-﻿var heroPoints = 10;
-var monsterPoints = 10;
+﻿using System.Diagnostics;
+
+static bool MonsterWins(int monsterPoints)
+{
+    return monsterPoints > 0;
+}
+
+static bool HeroWins(int heroPoints)
+{
+    return heroPoints > 0;
+}
 
 var damage = new Random();
-do
+
+int heroPoints = 10;
+int monsterPoints = 10;
+
+for (int i = 0; i < 100; i++)
 {
-    //hero attack
-    var monsterDamage = damage.Next(1, 11);
-    monsterPoints -= monsterDamage;
-    System.Console.WriteLine($"Monster: Lost {monsterDamage} Remaining {monsterPoints}");
+    heroPoints = 10;
+    monsterPoints = 10;
 
-    //monster attack
-    var heroDamage = damage.Next(1, 11);
-    heroPoints -= heroDamage;
-    System.Console.WriteLine($"Hero: Lost {heroDamage} Remaining {heroPoints}");
+    do
+    {
+        // hero attack
+        var monsterDamage = damage.Next(1, 11);
+        monsterPoints -= monsterDamage;
+        PrintDamage("Monster", monsterPoints, monsterDamage);
 
-} while (heroPoints > 0 && monsterPoints > 0);
+        // monster attack
+        var heroDamage = damage.Next(1, 11);
+        heroPoints -= heroDamage;
+        PrintDamage("Hero", heroPoints, heroDamage);
 
-if (heroPoints > 0)
-{
-    System.Console.WriteLine("Hero wins!");
+        Console.WriteLine();
+
+    } while (heroPoints > 0 && monsterPoints > 0);
+
 }
-if (monsterPoints > 0)
+
+// Mutually exclusive events
+Debug.Assert((HeroWins(heroPoints) && MonsterWins(monsterPoints)) == false, "Cannot be that both Hero and Monster win!");
+
+//Mutually exhaustive events
+Debug.Assert(!HeroWins(heroPoints) && !MonsterWins(monsterPoints) == false, "Cannot be that neither Hero nor Monster win!");
+
+if (HeroWins(heroPoints))
 {
-    System.Console.WriteLine("Monster wins!");
+    Console.WriteLine("Hero wins!");
 }
+if (MonsterWins(monsterPoints))
+{
+    Console.WriteLine("Monster wins!");
+}
+
+static void PrintDamage(string player, int playerPoints, int playerDamage)
+{
+    Console.Write($"{player}: ");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.Write("-" + playerDamage + " ");
+    Console.ResetColor();
+    Console.Write("-> ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write(playerPoints + "\n");
+    Console.ResetColor();
+}
+
+// static string Colorize(string text, ConsoleColor color)
+// {
+//     string colorCode = "\u001b[" + (int)color + "m";
+//     string resetCode = "\u001b[0m";
+//     return colorCode + text + resetCode;
+// }
