302f0eb3aba6085e6d4502ecefb6f01ff3d4fa20
sduzair <sduzairahmed@gmail.com>
2024-05-06 20:07:10 -0400

    Adds projects exploring exeption handling

---
+++ exceptions/csharpexceptions/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    int a = 4;
+    int b = 0;
+    int c = a / b;
+    System.Console.WriteLine($"{a} / {b} = {c}");
+}
+catch (Exception ex)
+{
+    // Console.WriteLine(ex.Message);
+    throw new ApplicationException("An error occurred", ex);
+}
+
+Console.ReadLine();
+
+// Hold Alt + 1 + 7 + 8 + Release Alt
+// ▓▓▓▓▓▓▓▓▓▓
+// ▓▓▓▓▓▓▓▓▓▓
+// ▓▓▓▓▓▓▓▓▓▓
