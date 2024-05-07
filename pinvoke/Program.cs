c8015dfc0914e84081ef55573d0c4fb6bb7e89db
sduzair <sduzairahmed@gmail.com>
2024-05-06 20:04:09 -0400

    Add projects exploring Windows api

---
+++ pinvoke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+  [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+  private static extern int MessageBox(int hWnd, string text, string caption, uint type);
+
+  private static void Main(string[] args)
+  {
+    MessageBox(0, "Hello World!", "Hello Dialog", 0);
+  }
+}
